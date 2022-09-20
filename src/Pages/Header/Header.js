2a9dc7b2921e657c2bca7ec1173a40594cb09aa5
Md Shahriur Ahmed <shahriurahmedcse@gmail.com>
2022-09-20 15:00:42 +0600

    Project setup

---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-primary text-start '>Hello</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FF0000",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+
+    ],
+  },
+  plugins: [require("daisyui")],
+}
