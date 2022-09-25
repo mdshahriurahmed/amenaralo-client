17bfe627ff5826d430b5312eb2c9fb4bd66c51ed
Md Shahriur Ahmed <shahriurahmedcse@gmail.com>
2022-09-25 10:37:07 +0600

    Lazy Loader removed from inner components

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,40 +1,39 @@
-import React, { Suspense } from 'react';
-import Loader from '../Loader/Loader';
-const Banner = React.lazy(() => import("./Banner/Banner"));
-const WhatWeDo = React.lazy(() => import('./WhatWeDo/WhatWeDo'));
-const Helped = React.lazy(() => import('./Helped/Helped'));
-const MakeDifference = React.lazy(() => import('./MakeDifference/MakeDifference'));
-const Gallery = React.lazy(() => import('../Gallery/Gallery'));
+import React from 'react';
+import Banner from "../Home/Banner/Banner"
+import WhatWeDo from "../Home/WhatWeDo/WhatWeDo"
+import Helped from "../Home/Helped/Helped"
+import MakeDifference from "../Home/MakeDifference/MakeDifference";
+import Gallery from "../Gallery/Gallery"
 
 const Home = () => {
     return (
         <div className='mt-16 z-0 '>
             {/* ---------Slider starts from here --------*/}
-            <Suspense fallback={<div><Loader></Loader></div>}>
-                <Banner />
-            </Suspense>
+
+            <Banner></Banner>
+
             {/* ---------Slider end here --------*/}
 
             {/* ---------What We Do Section starts from here --------*/}
-            <Suspense fallback={<div><Loader></Loader></div>}>
-                <WhatWeDo></WhatWeDo>
-            </Suspense>
+
+            <WhatWeDo></WhatWeDo>
+
             {/* ---------What We Do Section end here --------*/}
             {/* ---------We helped and we are Section starts from here --------*/}
-            <Suspense fallback={<div><Loader></Loader></div>}>
-                <Helped></Helped>
-            </Suspense>
+
+            <Helped></Helped>
+
             {/* ---------We helped and we are Section end here --------*/}
 
             {/* ---------How we work to make a difference Section starts from here --------*/}
-            <Suspense fallback={<div><Loader></Loader></div>}>
-                <MakeDifference></MakeDifference>
-            </Suspense>
+
+            <MakeDifference></MakeDifference>
+
             {/* ---------How we work to make a difference Section end here --------*/}
             {/* ---------How we work to make a difference Section starts from here --------*/}
-            <Suspense fallback={<div><Loader></Loader></div>}>
-                <Gallery></Gallery>
-            </Suspense>
+
+            <Gallery></Gallery>
+
             {/* ---------How we work to make a difference Section end here --------*/}
 
         </div>
